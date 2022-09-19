af493a31cb6162006c385aefde079d4016ebbba8
berkkusku <berkkusku@hotmail.com>
2022-09-20 00:12:48 +0300

    Log IN

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  email: string = "";
+  password: string = "";
+  check: boolean = false;
+  buttonChecked = true;
+
+  checkAvailable(){
+    if(this.email != "" && this.password != "" && this.check == true){
+      this.buttonChecked = false;}
+    else{
+      this.buttonChecked = true;
+    }
+
+    
+  }
+  
+
+}
